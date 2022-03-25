50092be73bdee883e00dd4aae870dd382730147a
FCR <franchesco2009@gmail.com>
2022-03-25 17:52:44 +0100

    modificación HTML y el JS, falta comprobar volteo de dos cartas y comprobar si son la misma figura

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 var flipOne = false;
 var flipTwo = false;
-    
-const card = document.getElementById("card");
-var figure = document.querySelector('span');
 
-function flipCard() { //REDEFINIR FUNCION
+let emojis = [0x1F600, 0x1F600, 0x1F604, 0x1F604, 0x1F34A, 0x1F34A, 0x1F344, 0x1F344,  0x1F37F, 0x1F37F, 0x1F363, 0x1F363, 
+                0x1F370, 0x1F370, 0x1F355, 0x1F355, 0x1F354, 0x1F354, 0x1F35F, 0x1F35F, 0x1F6C0, 0x1F6C0, 0x1F48E, 0x1F48E, 
+                0x1F5FA, 0x1F5FA, 0x23F0, 0x23F0, 0x1F579, 0x1F579,
+                0x1F431, 0x1F431, 0x1F42A, 0x1F42A, 0x1F439, 0x1F439, 0x1F424, 0x1F424];
+
+
+const card = document.getElementsByTagName('div');
+
+const cards = document.getElementsByClassName("cards");
+
+const figure = document.getElementsByTagName('span');
+
+const displayFig = document.querySelector('span');
+
+for(let i = 0 ; i < figure.length ; i++) {
+
+    figure[i].innerHTML = String.fromCodePoint(emojis[i]);;
+  
+}
+
+for(let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", flipCard);
+}
+
+var flipCard = function() { //REDEFINIR FUNCION
     card.classList.add('flip-vertical-right');
     card.classList.remove('back');
     card.classList.add('front');
-    figure.classList.toggle('dispNone');
+    displayFig.classList.toggle('dispNone');
 
+    flipOne = true;
 
     if(flipOne && flipTwo) { //REVISAR
         checkPairing();
@@ -18,27 +40,13 @@ function flipCard() { //REDEFINIR FUNCION
 
 function checkPairing() {
 
-    if(flipOne && flipTwo) {
         if(emojis[0] == emojis[1]) {
             console.log('¡Has encontrado pareja!');
         }
         else {
             console.log('No has encontrado pareja...');
         }
-    }
 }
 
-// let emojis = [0x1F600, 0x1F604, 0x1F34A, 0x1F344, 0x1F37F, 0x1F363, 0x1F370, 0x1F355,
-//     0x1F354, 0x1F35F, 0x1F6C0, 0x1F48E, 0x1F5FA, 0x23F0, 0x1F579, 0x1F4DA,
-//     0x1F431, 0x1F42A, 0x1F439, 0x1F424];
 
-// function fillCards(emojis) {
 
-//     for(let i = 0; emojis.lentgh ; i++) {
-//             cards.innerHTML = emojis[i];
-//     }
-// }
-
-// fillCards(emojis);
-
-//cards.innerText = String.fromCodePoint(0x1F354);
